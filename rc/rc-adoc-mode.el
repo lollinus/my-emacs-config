608cde0b23199ac061c715375bf8bad25a78f81c
Karol Barski <karol.barski@tieto.com>
2012-03-28 11:41:46 +0200

    Update adoc-mode config

--- rc/rc-adoc-mode.el
+++ rc/rc-adoc-mode.el
@@ -1,21 +1,15 @@
 ;;; rc-adoc-mode.el ---
 
 (add-to-list 'load-path (concat my-site-lisp-directory "adoc-mode"))
-(add-to-list 'load-path (concat my-site-lisp-directory "asciidoc-mode"))
 
 (autoload 'adoc-mode "adoc-mode")
 
 (setq auto-mode-alist
       (append '(
-                ("\\.adoc$" . doc-mode)
-                ("\\.asciidoc$" . doc-mode))
+                ("\\.adoc$" . adoc-mode)
+                ("\\.asciidoc$" . adoc-mode))
               auto-mode-alist
               )
       )
-
-(add-hook 'doc-mode-hook
-          '(lambda ()
-             (turn-on-auto-fill)
-             (require 'asciidoc)))
 
 ;;; rc-adoc-mode.el ends here
