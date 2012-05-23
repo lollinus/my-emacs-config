b2dd9d30ad5d5d1b93e39f43a5a4cd075cf9df02
Karol Barski <karol.barski@tieto.com>
2012-05-23 11:21:49 +0200

    Udate yasnippet
    Add org-addons project which aims at creating org-export backend for asciidoc

---
+++ rc/rc-org-addons.el
@@ -0,0 +1,8 @@
+;;; rc-org-addons.el ---
+(add-to-list 'load-path
+             (concat my-site-lisp-directory "org-addons"))
+
+(require 'org-mw)
+(require 'org-asciidoc)
+
+;;; rc-org-addons.el ends here
