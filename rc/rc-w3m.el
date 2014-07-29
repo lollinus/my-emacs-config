dd10dfed52968adda6982da5d6f6374b513a4d5f
Karol Barski <karol.barski@tieto.com>
2014-07-29 13:37:52 +0200

    Fix proxy configuration

--- rc/rc-w3m.el
+++ rc/rc-w3m.el
@@ -1,7 +1,17 @@
+;;; rc-w3m.el ---
 ;; configure proxy
 
 ;; (setq url-using-proxy t)
-;; (setq url-proxy-services '(("no_proxy" . "eu\\.tieto\\.com")
-;;                            ("http" . "http://10.28.10.14:8080")
-;;                            ("ftp" . "http://10.28.10.14:8080")
-;;                            ("https" . "http://10.28.10.14:8080")))
+;; (if running-ms-windows
+;;   	(progn (setq url-proxy-services '(("no_proxy" . "eu\\.tieto\\.com")
+;;   									  ("http" . "10.28.10.14:8080")
+;;   									  ("ftp" . "10.28.10.14:8080")
+;;   									  ("https" . "10.28.10.14:8080"))))
+;;   (progn
+;;   	(setq url-proxy-services '(("no_proxy" . "eu\\.tieto\\.com")
+;;   							   ("http" . "http://10.28.10.14:8080")
+;;   							   ("ftp" . "http://10.28.10.14:8080")
+;;   							   ("https" . "http://10.28.10.14:8080"))))
+;;   )
+
+;;; rc-w3m.el ends here
