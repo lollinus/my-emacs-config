6cd43cfbadbf271c99fd1ec19e24816ee0054b6a
Karol Barski <karol@barski.eu>
2014-08-18 13:40:02 +0200

    Cleanup: Remove python-mode
    Use elpa package instead

--- rc/rc-python.el
+++ rc/rc-python.el
@@ -1,17 +1,18 @@
 ;;; rc-python.el ---
 ;; python mode configuration
-(add-to-list 'load-path (concat my-site-lisp-directory "python-mode"))
 
-(setq python-python-command "python2.7")
-(setq auto-mode-alist
-      (cons '("\\.py$" . python-mode) auto-mode-alist))
-(setq interpreter-mode-alist
-      (cons '("python" . python-mode)
-            interpreter-mode-alist))
-(autoload 'python-mode "python-mode" "Python editing mode." t)
+(ensure-package-installed 'python-mode)
+
+;; (setq python-python-command "python2.7")
+;; (setq auto-mode-alist
+;;       (cons '("\\.py$" . python-mode) auto-mode-alist))
+;; (setq interpreter-mode-alist
+;;       (cons '("python" . python-mode)
+;;             interpreter-mode-alist))
+;; (autoload 'python-mode "python-mode" "Python editing mode." t)
 (add-hook 'python-mode-hook (lambda () (setq show-trailing-whitespace t)))
 (add-hook 'python-mode-hook 'font-lock-fontify-numbers)
-;(add-hook 'python-mode-hook (lambda () (require 'py-smart-operator)
-;                              (py-smart-operator-mode)))
+;; (add-hook 'python-mode-hook (lambda () (require 'py-smart-operator)
+;;                               (py-smart-operator-mode)))
 
 ;;; rc-python.el ends here
