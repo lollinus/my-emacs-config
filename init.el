5503d281f8c4999f83ca153979733b6fe4f926b7
Karol Barski <karol.barski@partner.bmwgroup.com>
2025-09-12 13:35:46 +0200

    cfg: org screenshot insert

--- init.el
+++ init.el
@@ -848,7 +848,20 @@
        (gnuplot . t)
        )))
   :hook ((org-mode-hook . kb/org-mode-setup))
-  )
+
+  :preface
+  (defun ews-org-insert-screenshot ()
+    "Take a screenshot with ImageMagick and insert as an Org mode link."
+    (interactive)
+    (let ((filename (read-file-name "Enter filename for screenshot: " default-directory)))
+      (unless (string-equal "png" (file-name-extension filename))
+        (setq filename (concat (file-name-sans-extension filename) ".png")))
+      (call-process-shell-command (format "import %s" filename))
+      (insert (format "#+caption: %s\n" (read-from-minibuffer "Caption: ")))
+      (insert (format "[[file:%s]]" filename))
+      (org-redisplay-inline-images)))
+  :bind (org-mode-map ("C-<print>" . ews-org-insert-screenshot))
+ )
 
 (leaf ox-jira
   :doc "JIRA Backend for Org Export Engine"
